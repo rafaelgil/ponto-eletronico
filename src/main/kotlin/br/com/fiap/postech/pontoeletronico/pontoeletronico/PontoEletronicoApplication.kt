8727f4f1bb25c0b589d06dba26744de0e0aeef5f
Rafael Carmona Gil <rafaelcarmonagil@gmail.com>
2024-03-19 15:53:32 -0300

    Projeto Ponto eletronico

---
+++ src/main/kotlin/br/com/fiap/postech/pontoeletronico/pontoeletronico/PontoEletronicoApplication.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.postech.pontoeletronico.pontoeletronico
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PontoEletronicoApplication
+
+fun main(args: Array<String>) {
+	runApplication<PontoEletronicoApplication>(*args)
+}
